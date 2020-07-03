6d9406b5313b867c25503f1b9c0c1f9d9058ca32
sherwin wyco <sherwin.wyco20@gmail.com>
2020-07-03 01:31:52 -0400

    remodified project structure

--- front-end/src/components/app/App.js
+++ front-end/src/components/app/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import logo from '../icons/logo.svg'
-import '../css/App.css'
+import logo from '../images/logo.svg'
+import './App.css'
 
 function App () {
   return (
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './assets/css/index.css'
-import App from './assets/components/App'
+import './index.css'
+import App from './components/app/App'
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(<React.StrictMode>
