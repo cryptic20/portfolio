9c4e55ac4551bcf5df0bc4a7f1f74068b1a38db3
sherwin wyco <sherwin.wyco20@gmail.com>
2020-07-03 08:48:26 -0400

    added redux store

--- front-end/src/components/app/App.js
+++ front-end/src/components/app/App.js
@@ -4,6 +4,7 @@ import { ThemeProvider , createMuiTheme } from '@material-ui/core/styles';
 import Switch from '@material-ui/core/Switch'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
 import CssBaseline from '@material-ui/core/CssBaseline';
+import NavBar from '../nav/NavBar'
 
 function App () {
   const [checked, setChecked] = React.useState(false);
@@ -27,6 +28,7 @@ function App () {
    <ThemeProvider  theme={theme}>
       <CssBaseline/>
       </ThemeProvider>
+        <NavBar />
             <FormControlLabel
             control={<Switch checked={checked} onChange={toggleChecked} />}
              label="dark"
